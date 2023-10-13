0038432e5ef66ef67d8477a3427d4c6f8995838f
markus <markus@ThinkPadMarkus.de>
2023-10-13 21:56:06 +0200

    improve increment handling in custom context menu
    * implement buttons and actions for separate sentence and
      part-of-sentence increments
    * smarter handling of increments in sentence / -parts when a less
      important offset exists (char, part-of-sentence)
      Note: still disabled for debugging

--- customcontextmenu.lua
+++ customcontextmenu.lua
@@ -74,8 +74,10 @@ function CustomContextMenu:init()
     -- create some helper functions
     local update = function(opts)
         self.prev_c_cnt = opts.prev_c or self.prev_c_cnt
+        self.prev_p_cnt = opts.prev_p or self.prev_p_cnt
         self.prev_s_cnt = opts.prev_s or self.prev_s_cnt
         self.next_c_cnt = opts.next_c or self.next_c_cnt
+        self.next_p_cnt = opts.next_p or self.next_p_cnt
         self.next_s_cnt = opts.next_s or self.next_s_cnt
         self:update_context()
     end
@@ -83,23 +85,80 @@ function CustomContextMenu:init()
     local can_append = function() return self.note.has_appended_content end
     local prev_c_inc = function(inc) update({ prev_c = self.prev_c_cnt + inc}) end
     local next_c_inc = function(inc) update({ next_c = self.next_c_cnt + inc}) end
-    -- char counter is reset to 0 when sentence count is changed
-    local prev_s_inc = function(inc) update({ prev_c = 0, prev_s = self.prev_s_cnt + inc}) end
-    local next_s_inc = function(inc) update({ next_c = 0, next_s = self.next_s_cnt + inc}) end
+    -- char counter is reset to 0 when sentence part count is changed
+    local prev_p_inc = function(inc)
+        local reset_is_sufficient = (inc > 0 and self.prev_c_cnt < 0) or (inc < 0 and self.prev_c_cnt > 0)
+        reset_is_sufficient = false -- DEBUG
+        if reset_is_sufficient then
+            -- just reset to next part-of-sentence if there is a character offset in the opposite direction 
+            update({ prev_c = 0})
+        else
+            update({ prev_c = 0, prev_p = self.prev_p_cnt + inc})
+        end
+    end
+    local next_p_inc = function(inc)
+        local reset_is_sufficient = (inc > 0 and self.prev_c_cnt < 0) or (inc < 0 and self.prev_c_cnt > 0)
+        reset_is_sufficient = false -- DEBUG
+        if reset_is_sufficient then
+            -- just reset to next part-of-sentence if there is a character offset in the opposite direction
+            -- Note: what about larger increments (currently not the case)? Will be eaten currently
+            update({ next_c = 0})
+        else
+            update({ next_c = 0, next_p = self.next_p_cnt + inc})
+        end
+    end
+    
+    local prev_s_inc = function(inc)
+        -- TODO: set unused prev cnts to 0
+        if inc == 0 then
+            return
+        end
+        local prev_ctx_current, next_ctx_current = self.note:get_custom_context(self.prev_s_cnt, self.prev_p_cnt, self.prev_c_cnt, self.next_s_cnt, self.next_p_cnt, self.next_c_cnt)
+        local prev_ctx_reset, next_ctx_reset = self.note:get_custom_context(self.prev_s_cnt, 0, 0, self.next_s_cnt, 0, 0)
+        local reset_is_sufficient = (inc > 0 and #next_ctx_reset > #next_ctx_current) or (inc < 0 and #next_ctx_reset < #next_ctx_current)
+        reset_is_sufficient = false -- DEBUG
+        if reset_is_sufficient then
+            -- If resetting part + char results will result in a context that is further in the desired direction, then just do that
+            -- Note: might also check if it is longer than the next sentence
+            -- Note: what about larger increments (currently not the case)? Will be eaten currently
+            update({ prev_c = 0, prev_p = 0})
+        else
+            update({ prev_c = 0, prev_p = 0, prev_s = self.prev_s_cnt + inc})
+        end
+    end
+
+    local next_s_inc = function(inc)
+        -- TODO: set unused prev cnts to 0
+        if inc == 0 then
+            return
+        end
+        local prev_ctx_current, next_ctx_current = self.note:get_custom_context(self.prev_s_cnt, self.prev_p_cnt, self.prev_c_cnt, self.next_s_cnt, self.next_p_cnt, self.next_c_cnt)
+        local prev_ctx_reset, next_ctx_reset = self.note:get_custom_context(self.prev_s_cnt, 0, 0, self.next_s_cnt, 0, 0)
+        local reset_is_sufficient = (inc > 0 and #next_ctx_reset > #next_ctx_current) or (inc < 0 and #next_ctx_reset < #next_ctx_current)
+        reset_is_sufficient = false -- DEBUG
+        if reset_is_sufficient then
+            -- If resetting part + char results will result in a context that is further in the desired direction, then just do that
+            -- Note: might also check if it is longer than the next sentence
+            -- Note: what about larger increments (currently not the case)? Will be eaten currently
+            update({ next_c = 0, next_p = 0})
+        else
+            update({ next_c = 0, next_p = 0, next_s = self.next_s_cnt + inc})
+        end
+    end
 
     -- buttons for editing previous context
-    local remove_prev_sentence = make_button("⏩", btn_width, function() prev_s_inc(-1) end, can_prepend)
-    local remove_prev_char =     make_button("1-", btn_width, function() prev_c_inc(-1) end, can_prepend)
-    local append_prev_char =     make_button("+1", btn_width, function() prev_c_inc(1) end)
-    local append_prev_sentence = make_button("⏪", btn_width, function() prev_s_inc(1) end)
-    local reset_prev =           make_button("Reset", btn_width*2, function() self:reset_prev(); return self:update_context() end)
+    local remove_prev_sentence_part = make_button("⏩", btn_width, function() prev_p_inc(-1) end , can_prepend)
+    local remove_prev_char =          make_button("1-", btn_width, function() prev_c_inc(-1) end, can_prepend)
+    local append_prev_char =          make_button("+1", btn_width, function() prev_c_inc(1) end)
+    local append_prev_sentence_part = make_button("⏪", btn_width, function() prev_p_inc(1) end)
+    local reset_prev =                make_button("Reset", btn_width*2, function() self:reset_prev(); return self:update_context() end)
 
     -- buttons for editing following context
-    local remove_next_sentence = make_button("⏪", btn_width, function() next_s_inc(-1) end, can_append)
-    local remove_next_char =     make_button("-1", btn_width, function() next_c_inc(-1) end, can_append)
-    local append_next_char =     make_button("1+", btn_width, function() next_c_inc(1) end)
-    local append_next_sentence = make_button("⏩", btn_width, function() next_s_inc(1) end)
-    local reset_next =           make_button("Reset", btn_width*2, function() self:reset_next(); self:update_context() end)
+    local remove_next_sentence_part = make_button("⏪", btn_width, function() next_p_inc(-1) end, can_append)
+    local remove_next_char =          make_button("-1", btn_width, function() next_c_inc(-1) end, can_append)
+    local append_next_char =          make_button("1+", btn_width, function() next_c_inc(1) end)
+    local append_next_sentence_part = make_button("⏩", btn_width, function() next_p_inc(1) end)
+    local reset_next =                make_button("Reset", btn_width*2, function() self:reset_next(); self:update_context() end)
 
     -- holding the ±1 buttons allows to jump back and forth in larger increments 
     remove_prev_char.hold_callback = function() prev_c_inc(- self.conf.custom_context_jump_size:get_value()) end
@@ -107,28 +166,38 @@ function CustomContextMenu:init()
     remove_next_char.hold_callback = function() next_c_inc(- self.conf.custom_context_jump_size:get_value()) end
     append_next_char.hold_callback = function() next_c_inc(  self.conf.custom_context_jump_size:get_value()) end
 
+    -- holding the << / >> buttons makes them apply sentences instead of parts of sentences
+    remove_prev_sentence_part.hold_callback = function() prev_s_inc(-1) end
+    append_prev_sentence_part.hold_callback = function() prev_s_inc( 1) end
+    remove_next_sentence_part.hold_callback = function() next_s_inc(-1) end
+    append_next_sentence_part.hold_callback = function() next_s_inc( 1) end
+
     self.top_row = HorizontalGroup:new{
         align = "center",
-    append_prev_sentence,
+    append_prev_sentence_part,
     append_prev_char,
     reset_prev,
     remove_prev_char,
-    remove_prev_sentence,
+    remove_prev_sentence_part,
     }
 
     self.bottom_row = HorizontalGroup:new{
         align = "center",
-    remove_next_sentence,
+    remove_next_sentence_part,
     remove_next_char,
     reset_next,
     append_next_char,
-    append_next_sentence,
+    append_next_sentence_part,
     }
 
+    local save_btn = make_button("Save with custom context", btn_width*4, self.on_save_cb)
+    save_btn.hold_callback = function()
+        self.ui.AnkiWidget:show_config_widget() -- TODO: reference correct AnkiWidget
+    end
     self.confirm_row = HorizontalGroup:new{
         align = "center",
         make_button("Cancel", btn_width*2, function() self:onClose() end),
-        make_button("Save with custom context", btn_width*4, self.on_save_cb),
+        save_btn,
     }
 
     self.context_menu = FrameContainer:new{
@@ -193,16 +262,18 @@ end
 
 function CustomContextMenu:reset_prev()
     self.prev_s_cnt = 1
+    self.prev_p_cnt = 0
     self.prev_c_cnt = 0
 end
 
 function CustomContextMenu:reset_next()
     self.next_s_cnt = 1
+    self.next_p_cnt = 0
     self.next_c_cnt = 0
 end
 
 function CustomContextMenu:update_context()
-    local prev, next_ = self.note:get_custom_context(self.prev_s_cnt, self.prev_c_cnt, self.next_s_cnt, self.next_c_cnt)
+    local prev, next_ = self.note:get_custom_context(self.prev_s_cnt, self.prev_p_cnt, self.prev_c_cnt, self.next_s_cnt, self.next_p_cnt, self.next_c_cnt)
     local css = [[
         h2 {
             display: inline;
